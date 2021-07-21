18ae05920c93934d64d4a81cfacc92537cf5a3f0
CristiScarlat <78.cristi@gmail.com>
2021-07-21 13:31:01 +0300

    fixed auth only for login, fixed componentdidmount

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = () => {
         }
     })
 
- })
+ },[])
 
  function handleLogout() {
    auth.signOut()
--- src/pages/modules/auth.js
+++ src/pages/modules/auth.js
@@ -4,39 +4,23 @@ import RegisterLoginForm from '../components/registerForm';
 
 const Auth = () => {
     const [error, setError] = React.useState('');
-    const [authType, setAuthType] = React.useState('login');
-
-    const registerTextBtn="Or register if you do not have an account yet"
-    const loginTextBtn="and go to login"
 
     const handleFormSubmit = async (email, psw) => {
         try {
-            if(authType === 'login'){
-                const res = await auth.signInWithEmailAndPassword(email, psw)
-            } else if(authType === 'register'){
-                const res = await auth.createUserWithEmailAndPassword(email, psw)
-            } 
+            await auth.signInWithEmailAndPassword(email, psw)
         } catch (error) {
             setError(error.message || "Somethin went wrong, please try again!")
         }
 
     }
 
-    const handleSwitchRegisterLogin = () => {
-        if(authType === 'login')setAuthType('register')
-        else setAuthType('login')
-    }
-
     return (
         <div className="login-container">
             <div className="auth-container">
-                <div className="auth-title">{authType}</div>
+                <div className="auth-title">Login:</div>
                 <RegisterLoginForm handleFormSubmit={handleFormSubmit} />
             </div>
             <div style={{ color: 'red', marginTop: '1rem'}}>{error}</div>
-            <button onClick={handleSwitchRegisterLogin} className="register-button">
-                {authType === 'login' ? registerTextBtn : loginTextBtn}
-            </button>
         </div>
     )
 }
